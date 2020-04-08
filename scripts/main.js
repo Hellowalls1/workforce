ca818c81d8cf609fc98168686cc0ff57719915d5
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-08 16:06:15 -0500

    added departments

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import { EmployeeList } from "./employee/employeeList.js";
 import { getEmployees } from "./employee/employeeDataProvider.js";
 import { getComputers } from "./computer/computerDataProvider.js";
+import { getDepartments } from "./department/departmentDataProvider.js";
 
 getEmployees()
     .then(getComputers)
+    .then(getDepartments)
     .then(EmployeeList)
