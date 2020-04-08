020d05f4bb2aa20a58616734e52f3541a87d4095
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-08 10:19:05 -0500

    fixed error

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ import { EmployeeList } from "./employee/employeeList.js";
 import { getEmployees } from "./employee/employeeDataProvider.js";
 import { getComputers } from "./computer/computerDataProvider.js";
 
-
-EmployeeList()
+getEmployees()
     .then(getComputers)
-    .then(getEmployees)
+    .then(EmployeeList)
