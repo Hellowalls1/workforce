e633317928773347b5ab8be8eb00e31dcb3b85e5
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-09 09:14:22 -0500

    added some comments

--- scripts/computer/computerDataProvider.js
+++ scripts/computer/computerDataProvider.js
@@ -1,8 +1,9 @@
 let computers = []
 
+//copy of the data 
 export const useComputers = () => computers.slice()
 
-
+//getting the data from the api
 export const getComputers = () => fetch("http://localhost:8088/computers")
     .then(res => res.json())
     .then(data => computers = data)
--- scripts/employee/employeeList.js
+++ scripts/employee/employeeList.js
@@ -4,10 +4,15 @@ import { employee } from "./Employee.js"
 import { useDepartments } from "../department/departmentDataProvider.js"
 import { useOffices } from "../offices/officeDataProvider.js"
  
+//where the things will end up
 const contentTarget = document.querySelector(".employees")
 
+
+//the render function will eventually use all of the emploiyee data. 
+//It will map over each employee and use find to obtain information about the employees. 
+//It needs all the data before it can do the things
     const render = employeesToRender => {
-        //getting the data
+        //getting a copy of the data and storing it into variables
         const computers = useComputers()
         const departments = useDepartments()
         const offices = useOffices()
@@ -20,11 +25,13 @@ const contentTarget = document.querySelector(".employees")
                 return computer.id === employeeObject.computerId //return the computer.Id = the computerId on employee object in array
             }
             )
+            //Find the related department for the curent employee
             const foundDepartment = departments.find(
                 (department) => {
                 return department.id === employeeObject.departmentId
                 }
             )
+            //Finding the related office for the current employee
             const foundOffice = offices.find(
                 (office) => {
                 return office.id === employeeObject.officeId
@@ -36,9 +43,9 @@ const contentTarget = document.querySelector(".employees")
         ).join("")
     }
 
- 
+ //the EmployeeList function brings in a copy employee data and runs it through the render function
 export const EmployeeList = () => {
-    const employees = useEmployees()
+    const employees = useEmployees() //getting the copy of data and defining it in a variable to use in the render function 
     
     render(employees)
 }
